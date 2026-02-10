4325a2dd6bf358e080a0846c72df8b5de5fb1a4e
KellyJeanneBeck <kbe.kellybeck@gmail.com>
2026-02-10 09:05:18 +0100

    add calculate function

--- script.js
+++ script.js
@@ -29,6 +29,19 @@ function primitiveCalc(operator) {
 // --- Simple Calculator ---
 const calcDisplay = document.getElementById("calc_display");
 
+function simpleCalc() {
+  // replace() method to strip value of anything that isn't a digit, + - * / . or ()
+  let result = calcDisplay.value.replace(/[^-()\d/*+.]/g, "");
+  // eval() can be dangerous, that's why we clean the value with regex first
+  calcDisplay.value = eval(result);
+}
+
+calcDisplay.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    simpleCalc();
+  }
+});
+
 var addInDisplay = (btnValue) => (calcDisplay.value += btnValue);
 var clearDisplay = () => (calcDisplay.value = "");
 var deleteLast = () => (calcDisplay.value = calcDisplay.value.slice(0, -1));
